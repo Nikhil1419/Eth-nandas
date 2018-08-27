2fd2c6e778bf2ed325e11d8ef9e8e0050d22ad31
Nikhil Sahare <nikhilsahare8@gmail.com>
2018-08-27 17:40:44 +0530

    1. Dapp Test

---
+++ truffle-config.js
@@ -0,0 +1,17 @@
+
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+ 
+  network: {
+    development: {
+       host: "127.0.0.1",
+       port: "9545",
+       network_id: "*" //match any network id
+    }
+  }
+
+
+
+};
