9a5391987a64399ffdd71e652ab6fe1b21224661
Nikhil Sahare <nikhilsahare8@gmail.com>
2018-08-28 18:30:30 +0530

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,36 @@
 pragma solidity ^0.4.2;
 
 contract DappToken{
-    // Constructor
-    // Set the total number of tokens
-    // Read the total number of tokens
-    uint256 public totalSupply;
     
-    function DappToken () public {
-       totalSupply = 1000000;
+    string public name = "Nandas Token";
+    string public symbol = "NANDAS";
+    string public standard = "Nandas Token v1.0";
+    uint256 public totalSupply;
 
+    event Transfer(
+     address indexed _form,
+     address indexed _to,
+     uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    
+    function DappToken (uint256 _initialSupply) public {
+       balanceOf[msg.sender]= _initialSupply; 
+       totalSupply = _initialSupply;
+    
     }
-}
+    
+  // Transfer
+  function transfer(address _to, uint256 _value) public returns (bool success){
+       require(balanceOf[msg.sender] >= _value);
+    
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    Transfer(msg.sender, _to, _value);
+    
+    return true;
+  }
+    }
+
